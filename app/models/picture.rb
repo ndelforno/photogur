e8bbbaa6607db5c980348b13b0d8b5e90fa5c6e8
Ndelforno <nicolas.delforno@gmail.com>
2018-10-01 17:10:42 -0400

    exercise done

--- app/models/picture.rb
+++ app/models/picture.rb
@@ -12,7 +12,16 @@ class Picture < ApplicationRecord
     Picture.where("created_at < ?", Time.now - 1.month)
   end
 
-  def pictures_created_in_year(year)
-  end
+  # def pictures_created_in_year(year)
+  #   @pictures.each do |picture|
+  #     if picture.created_at.year == year
+  #       return picture
+  #     end
+  # end
 
+  validates :artist, presence: true
+  validates :title, length: { minimum: 2}
+  validates :title, length: { maximum: 20}
+  validates :url, presence: true
+  validates :url, uniqueness: true
 end
