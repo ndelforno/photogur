e228af52b385e8a69ef2f309b3f70fc081924ac5
Ndelforno <nicolas.delforno@gmail.com>
2018-10-04 15:53:55 -0400

    exercise done

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,8 @@
 Rails.application.routes.draw do
 
   resources :pictures
+  resources :users
+  resources :sessions
+  root 'pictures#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
